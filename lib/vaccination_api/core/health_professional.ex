8922387304c730d811471aeb3997584f60431810
Vitor Guimaraes <vitorrguimaraess@gmail.com>
2024-01-26 22:22:57 -0300

    minor fixes

--- lib/vaccination_api/core/health_professional.ex
+++ lib/vaccination_api/core/health_professional.ex
@@ -5,8 +5,7 @@ defmodule VaccinationApi.Core.HealthProfessional do
     User who applies vaccination to persons
   """
 
-  alias VaccinationApi.Utils
-  alias VaccinationApi.Core.{Vaccination}
+  alias VaccinationApi.Core.Vaccination
   use VaccinationApi.Schema
 
   @fields [:first_name, :last_name, :cpf, :professional_register, :email, :__password__]
@@ -27,17 +26,51 @@ defmodule VaccinationApi.Core.HealthProfessional do
     end
   end
 
-    def changeset(model \\ %__MODULE__{}, attrs) do
+  def changeset(model \\ %__MODULE__{}, attrs) do
     model
     |> cast(attrs, @fields)
-    |> validate_format(:email, ~r/@/)
-    |> Utils.validate_cpf()
+    |> validate_format(:email, ~r/@/, message: "invalid email format")
+    |> validate_cpf()
     |> unique_constraint(:cpf)
-    |> Utils.put_password_hash()
+    |> put_password_hash()
   end
 
   def changeset_insert(model, attrs), do: changeset_(model, attrs, :insert)
   def changeset_update(model, attrs), do: changeset_(model, attrs, :update)
+
+  defp validate_cpf(%{changes: %{cpf: cpf}} = changeset) do
+    cond do
+      Brcpfcnpj.cpf_valid?(cpf) == false ->
+        Ecto.Changeset.add_error(changeset, :cpf, "cpf is invalid")
+
+      :else ->
+        changeset
+    end
+  end
+
+  defp put_password_hash(%{changes: %{__password__: password}} = changeset) do
+    if password && changeset.valid? do
+      changeset
+      |> validate_length(:__password__,
+        min: 8,
+        message: "Password must be at least eight characters"
+      )
+      |> validate_format(:__password__, ~r/[0-9]+/, message: "Password must contain a number")
+      |> validate_format(:__password__, ~r/[A-Z]+/,
+        message: "Password must contain an upper-case letter"
+      )
+      |> validate_format(:__password__, ~r/[a-z]+/,
+        message: "Password must contain a lower-case letter"
+      )
+      |> validate_format(:__password__, ~r/[#\!\?&@\$%^&*\(\)]+/,
+        message: "Password must contain a symbol"
+      )
+      |> put_change(:hashed_password, Bcrypt.hash_pwd_salt(password))
+      |> delete_change(:__password__)
+    else
+      changeset
+    end
+  end
 
   defmodule Api do
     @moduledoc false
--- lib/vaccination_api/core/person.ex
+++ lib/vaccination_api/core/person.ex
@@ -2,17 +2,18 @@ defmodule VaccinationApi.Core.Person do
   @moduledoc """
     Person Entity
     ---
-    User Person to access vaccination platform
+    User Person to access vaccination platform as person who will take vaccine
   """
 
-  alias VaccinationApi.Utils
-  alias VaccinationApi.Core.{Vaccination}
+  alias VaccinationApi.Core.Vaccination
   use VaccinationApi.Schema
 
   @gender_enum [:male, :female]
-  @fields [:first_name, :last_name, :birth, :cpf, :sus_number, :mother_name, :gender, :email, :__password__]
+  @basic_fields [:id, :first_name, :last_name, :birth, :cpf, :sus_number, :mother_name, :gender, :email]
 
   generate_bee do
+    permission(:basic, @basic_fields)
+
     schema "person" do
       field :first_name, :string, bee: [required: true]
       field :last_name, :string, bee: [required: true]
@@ -31,19 +32,29 @@ defmodule VaccinationApi.Core.Person do
     end
   end
 
-  def changeset(model \\ %__MODULE__{}, attrs) do
-    model
-    |> cast(attrs, @fields)
-    |> validate_format(:email, ~r/@/)
-    |> Utils.validate_cpf()
+  def changeset_insert(model, attrs) do
+    changeset_(model, attrs, :insert)
+    |> validate_format(:email, ~r/@/, message: "invalid email format")
+    |> validate_cpf()
     |> unique_constraint(:cpf)
     |> unique_constraint(:sus_number)
-    |> validate_length(:sus_number, is: 15, message: "SUS number must be fifteen digits")
+    |> unique_constraint(:email)
+    |> validate_length(:sus_number, is: 15, message: "SUS number must have fifteen digits")
     |> put_password_hash()
   end
 
   def changeset_insert(model, attrs), do: changeset_(model, attrs, :insert)
   def changeset_update(model, attrs), do: changeset_(model, attrs, :update)
+
+  defp validate_cpf(%{changes: %{cpf: cpf}} = changeset) do
+    cond do
+      Brcpfcnpj.cpf_valid?(cpf) == false ->
+        Ecto.Changeset.add_error(changeset, :cpf, "cpf is invalid")
+
+      :else ->
+        changeset
+    end
+  end
 
   defp put_password_hash(%{changes: %{__password__: password}} = changeset) do
     if password && changeset.valid? do
@@ -59,6 +70,7 @@ defmodule VaccinationApi.Core.Person do
       changeset
     end
   end
+
 
   defmodule Api do
     @moduledoc false
