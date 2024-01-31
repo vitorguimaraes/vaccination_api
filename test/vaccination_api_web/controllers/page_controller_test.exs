64b4db15c76c7a8aef0481913a3a6c81834c98f3
Vitor Guimaraes <vitorrguimaraess@gmail.com>
2024-01-30 23:35:48 -0300

    fix: delete unused tests

--- test/vaccination_api_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule VaccinationApiWeb.PageControllerTest do
-  use VaccinationApiWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
