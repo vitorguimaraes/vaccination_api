5c599abb425bfc580dc2053decdb718b7b030776
Vitor Guimaraes <vitorrguimaraess@gmail.com>
2024-02-14 09:30:26 -0300

    chore: add jason lib

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule VaccinationApi.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
-      {:jason, "~> 1.2"},
+      {:jason, "~> 1.4"},
       {:plug_cowboy, "~> 2.5"},
 
       # other third party dependencies
